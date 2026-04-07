357e0509212caf81041e1ecee122289e1af47f41
Aman yadav <amanku12090@gmail.com>
2026-04-07 07:25:51 +0530

    Create operators.js with operator examples
    Add operators.js file with examples of various operators.

---
+++ operators.js
@@ -0,0 +1,104 @@
+//  Artimatic operator
+
+// let a =5;
+// let b =2;
+
+// console.log("a =", a , "& b =" ,b);
+// console.log("a + b =", a+b);
+// console.log("a - b =", a-b);
+// console.log("a * b =", a*b);
+// console.log("a / b =", a/b);
+// console.log("a % b =", a%b);
+// console.log("a ** b =", a**b);
+
+// unary operator
+
+// let a =5;
+// let b =2;
+// console.log("a =", a , "& b =" ,b);
+
+// a++;
+// console.log("a++ =", a++);
+// console.log("--a =", --a);
+
+// Assignment operators
+
+// let a =5;
+// let b =8;
+// a +=4;
+//  console.log("a =", a);
+
+// comparsion operators
+
+// let a =4;
+// let b =3;
+
+// console.log("4 >3", a>b);
+
+
+// logical operators
+
+// && ---- AND operators   || ---- OR operatores  ! ---- NOT operators
+
+// let a = 4;
+// let b = 5;
+
+// cond1 = b > a;
+// cond2 = b < a;
+
+// console.log(" cond1 && cond2 =", cond1 && cond2);
+// console.log(" cond1 || cond2 =", cond1 || cond2);
+// console.log(" !{4 < 5} =", !(a < b));
+
+// --- conditional statement ---
+
+// let age = 44;
+
+// if(age >= 18){
+//     console.log("you can eligibal to DL");
+// }
+// else if (age < 18){
+//     console.log("you can not eligibal to DL");
+// }
+
+// let mode = "light";
+// let color;
+
+// if (mode === "dark"){
+//     color ="black";
+// }
+
+// if (mode ==="light"){
+//     color ="white";
+// }
+
+// console.log(color);
+
+// let num =0;
+
+// if(num % 2=== 0){
+//     console.log(num, "is even");
+// }
+// else {
+//     console.log(num, "is odd");
+// }
+
+// --- Ternary operators ---
+
+// let age =14;
+
+// let result = age >= 18 ? "adult" : "not adult";
+// console.log(result);
+ 
+// --- practice question ---
+
+// let num = prompt("Enter number:");
+ 
+
+// if(num % 5 ===0){
+//     console.log(num ,"is multiple of 5 ");
+// }
+// else{
+//     console.log(num ,"is not multiple of 5" );
+// }
+
