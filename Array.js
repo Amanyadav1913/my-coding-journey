b95691d916af1585ede5e07801e9881201465a84
Aman yadav <amanku12090@gmail.com>
2026-04-09 17:19:00 +0530

    Add commented-out code examples in Array.js

---
+++ Array.js
@@ -0,0 +1,24 @@
+ // let arr = ["Apple","Banana","papaya","coconut","Grapes"];
+//  console.log(arr);
+
+//  let items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//  console.log(items.length)
+
+//  let nums = [1, 2, 3]
+
+//  console.log( nums.push(4));
+
+//  let lern = [1, 2, 3, 4, 5];
+//  let sum = 0;
+//  for (let i = 0; i < lern.length; i++) {
+//      sum += lern[i];
+    
+//  }
+//  console.log("sum =", sum)
+
+//  let marks = [45, 78, 32, 90, 55];
+ 
+//  for (let j = 0; j < marks.length; j++) {
+//     marks > array[j];
+    
+//  }
